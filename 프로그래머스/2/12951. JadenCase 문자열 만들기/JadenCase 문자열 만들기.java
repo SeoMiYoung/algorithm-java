7cd83e420cdddfc71b687462ce2e324a6499f25b
SeoMiYoung <112063987+SeoMiYoung@users.noreply.github.com>
2026-06-21 00:48:31 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.47 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,38 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-// [주의] 공백문자가 연속해서 나올 수 있습니다.
-//    split은 '연속된 공백'이나 '문자열 끝의 공백'처리가 까다로움 --> split 사용은 피해야함
-//    그래서 문자열을 직접 쪼개는 방식보다는 문자 하나씩 순회하며 직전 문자가 공백이었는지 확인하는 flag 방식을 써야함 
-
+// 음 결국에, 그냥 각 단어 앞에만 대문자로 바꾸라는 거 아닌가?
+// 나머지는 소문자고?
+// s는 알파벳과 숫자, 공백문자(" ")로 이루어져 있습니다.
 
 class Solution {
-    static StringBuilder sb;
-    
     public String solution(String s) {
-        sb = new StringBuilder();
+        String lowerS = s.toLowerCase(); // 모두 소문자화
+        String[] wordArr = lowerS.split(" ", -1); // ["3people", "unfollowed", "me"], -1로 뒤에 공백 보존 --> 즉, 마지막이 ""가 될수도 있음! 
         
-        s = s.toLowerCase(); // 모두 소문자로 변환 후 시작
-        boolean flag = true; // 단어의 첫 시작인가?
-        String answer = "";
-        
-        char[] words = s.toCharArray(); // [3, p, e, o, p, l, e,  , u, n, f, o, l, l, o, w, e, d,  , m, e]
-        for (char c : words) {
-            if (c == ' ') { // 공백 나오면 그대로 넣어줌
-                sb.append(c);
-                flag = true; // 유지
+        for (int idx=0; idx<wordArr.length; idx++) { // "3people", "unfollowed", "me" 이렇게 한 단어씩 getWord로 들어옴
+            if (!wordArr[idx].isEmpty()) { // 만약에 ""이라면, charAt에서 에러날 수 있음 
+                // 가장 맨 앞글자만 대문자로 바꿔야함
+                wordArr[idx] = Character.toUpperCase(wordArr[idx].charAt(0)) + wordArr[idx].substring(1);
             }
-            else {
-                if (flag) {  // 첫 글자
-                    sb.append(Character.toUpperCase(c)); // 대문자로 넣는다. (숫자여도 toUpperCase는 에러안남)
-                    flag = false;
-                }
-                else {  // 첫 글자가 아님
-                    sb.append(c);
-                }
-            }
-            
         }
-        return sb.toString();
+        
+        String answer = String.join(" ", wordArr);
+        return answer;
     }
 }
